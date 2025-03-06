43900eb4d9d64c70271ba059a7f300ffbdd0ecb7
elienecaldeira <eliene.rocha07@gmail.com>
2025-03-06 13:03:32 -0300

    Criacao da funcao adicionarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos=[];
+
+function adicionarAmigo(){
+
+    let inputAmigo =document.getElementById('amigo');
+    let nomeAmigo = inputAmigo.value;
+    console.log(nomeAmigo);
+    if(!nomeAmigo){
+        alert("Digite o nome do amigo");
+        return;
+    }
+    //verifica se o nome do amigo ja existe na lista
+
+    amigos.push(nomeAmigo);
+    console.log(amigos);
+    inputAmigo.value="";
+    inputAmigo.focus();
+
+}
